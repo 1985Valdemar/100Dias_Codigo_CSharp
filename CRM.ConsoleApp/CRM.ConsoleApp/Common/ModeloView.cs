564bf9c6cd523530a35a1ef48baa9a977a86fe0f
1985Valdemar <vteider@yahoo.com>
2025-01-09 21:07:08 -0300

    1 de 100 Atualizando Common

--- CRM.ConsoleApp/CRM.ConsoleApp/Common/ModeloView.cs
+++ CRM.ConsoleApp/CRM.ConsoleApp/Common/ModeloView.cs
@@ -8,5 +8,29 @@ namespace CRM.ConsoleApp.Common
 {
     internal class ModeloView
     {
+
+        //****** METODO LIMPAR & MOSTRAR CABECALHO ******
+        public static void Cabecalho(string titulo)
+        {
+            Console.WriteLine(titulo);
+            Thread.Sleep(3000);
+
+        }
+
+        //****** METODO mostrar e limpar msg ******
+
+        public static void Mensagem(string texto)
+        {
+            Console.Clear();
+            Console.WriteLine(texto);
+        }
+
+        //****** METODO LIMPAR ******
+
+        public static void Limpar()
+        {
+            Thread.Sleep(3000);
+            Console.Clear();
+        }
     }
 }
