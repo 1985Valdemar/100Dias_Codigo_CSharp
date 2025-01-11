931fcaf0025c104ad0ef795f50c07f68bd6e4962
1985Valdemar <vteider@yahoo.com>
2025-01-11 20:49:53 -0300

    Dia 3 de 100 = Criado Metodos CRUD e Utilizado View para salvar e recuperar TXT

--- CRM.ConsoleApp/CRM.ConsoleApp/Common/MetodosViews.cs
+++ CRM.ConsoleApp/CRM.ConsoleApp/Common/MetodosViews.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CRM.ConsoleApp.Common
 {
-    internal class ModeloView
+    internal class MetodosViews
     {
 
         //****** METODO LIMPAR & MOSTRAR CABECALHO ******
