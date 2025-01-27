10a1f44393aaf243f296250a2fa3812cd06f5d0f
1985Valdemar <vteider@yahoo.com>
2025-01-27 19:28:46 -0300

    Dia 19 de 100 = Atualizando projeto web para salvar banco dados ainda nao funcionou mas vai

---
+++ CRM.ConsoleApp/WebApplication2/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; } // Exemplo de uma entidade chamada Usuario
+    }
+}
