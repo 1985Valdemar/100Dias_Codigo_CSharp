6b5083bfad9d32089e5a327925901537d7cc449a
1985Valdemar <vteider@yahoo.com>
2025-01-31 19:22:05 -0300

    Dia 23 de 100 = Pagina Web MVC do zero

---
+++ CRM.ConsoleApp/WebApplication3/Data/BancoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class BancoContext : DbContext
+    {
+        //CONSTRUTOR DO CONTEXTO DO BANCO DADOS
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+        //TRAZENDO ENTIDADE MODELO DO CONTATO
+        //PROPRIEDADE VAI CRIAR TABELA
+        public DbSet<ContatoModel> Contato { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ContatoModel>()
+                .HasKey(c => c.Id); // Define a chave primária explicitamente, se necessário
+        }
+    }
+}
--- CRM.ConsoleApp/WebApplication3/Program.cs
+++ CRM.ConsoleApp/WebApplication3/Program.cs
@@ -1,27 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebApplication3.Data;
+using WebApplication3.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adicionando o serviço do banco de dados PostgreSQL
+builder.Services.AddDbContext<BancoContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+           .EnableSensitiveDataLogging() // Habilita o registro de dados sensíveis (opcional)
+           .LogTo(Console.WriteLine, LogLevel.Information)); // Registra no console
+
+// Adicionando os serviços do MVC (Controllers e Views)
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IContatoRepository, ContatoRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuração do pipeline de requisição HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
+// Configurando a rota padrão
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
